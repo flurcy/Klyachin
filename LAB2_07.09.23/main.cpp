fc01c915051d0cc9d66e7ae9ff9f8de4ac90240a
Anna <72861380+flurcy@users.noreply.github.com>
2023-10-10 23:21:29 +0300

    Update main.cpp

--- LAB2_07.09.23/main.cpp
+++ LAB2_07.09.23/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <utility>
-#include <windows.h>
 using namespace std;
 
+
+//функция для проверки знаменателя
+bool CheckNonZeroDenominator(int denominator) {
+    if (denominator == 0) {
+        std::cout << "Ошибка! Знаменатель не может быть равен нулю." << std::endl;
+        return true;
+    }
+    return false;
+}
+
+//функция для проверки ввода пользователя
+bool HandleInputError(const std::string& messageErrorInput) {
+    if (!std::cin.good()) {
+        std::cout << messageErrorInput;
+        return true;
+    }
+    return false;
+}
+
 //функция для нахождения НОД чисел
-int NOD(int a, int b) {
+int NOD(int a, int b) { 
     while (b != 0) {
         int temp = b;
         b = a % b;
@@ -14,13 +32,13 @@ int NOD(int a, int b) {
 }
 
 //функция для нахождения НОК чисел
-int NOK(int a, int b) {
+int NOK(int a, int b) { 
     return (a * b) / NOD(a, b);
 }
 
 //функция для разложения на множители
-int Factorization(int num) {
-    cout << "Множители числа " << num << ": ";
+int Factorization(int num) { 
+    std::cout << "Множители числа " << num << ": ";
     for (int divider = 2; divider <= num; divider++) {
         while (num % divider == 0) {
             std::cout << divider << " ";
@@ -64,90 +82,115 @@ pair<int, int> divideFractions(pair<int, int> fraction1, pair<int, int> fraction
     return simplifyFraction(newNumerator, newDenominator);
 }
 
-int main() {
-    setlocale(LC_ALL, "Russian");
 
+
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
     int num1, denom1, num2, denom2; //для дробей
     int num, a, b; //для чисел
+    int resultCalculations; //результат вычисления
 
     string messageInputNumber = "Введите числа a и b: ";
-    string messageResult = "Результат: ";
     string messageError = "Неверная операция!";
-
+    string messageErrorInput = "Ошибка! Некорректный ввод. Введите целые числа.";
+    
     char decision;
-    cout << "Над каким типом вы хотите проделать операцию? Введите 1, если над числом. Введите 2, если над дробью: ";
-    cin >> decision;
-
+    std::cout << "Над каким типом вы хотите проделать операцию? Введите 1, если над числом. Введите 2, если над дробью: ";
+    std::cin >> decision;
+        
     switch (decision) {
-    case '1':
-        char operationNum;
-        cout << "Выберите операцию: NOD (1), NOK (2), Factorization (3): ";
-        cin >> operationNum;
-        switch (operationNum) {
         case '1':
-            cout << messageInputNumber;
-            cin >> a;
-            cin >> b;
+            char operationNum;
+            std::cout << "Выберите операцию: NOD (1), NOK (2), Factorization (3): ";
+            std::cin >> operationNum;
+            switch (operationNum) {
+            case '1':
+                std::cout << messageInputNumber;
+                std::cin >> a;
+                std::cin >> b;
 
-            std::cout << messageResult << NOD(a, b);
-            break;
+                if (HandleInputError(messageErrorInput)) {
+                    return 1;
+                }
+
+                cout << "НОД чисел: " << NOD(a, b);
+
+                break;
+            case '2':
+                std::cout << messageInputNumber;
+                std::cin >> a;
+                std::cin >> b;
+
+                if (HandleInputError(messageErrorInput)) {
+                    return 1;
+                }
+
+                cout << "НОК чисел: " << NOK(a, b);
+
+                break;
+            case '3':
+                std::cout << "Введите число, которое вы хотите разложить на множители: ";
+                std::cin >> num;
+
+                if (HandleInputError(messageErrorInput)) {
+                    return 1;
+                }
+
+                cout << Factorization(num);
+
+                break;
+            default:
+                std::cout << messageError;
+            }
+            return 0;
+
         case '2':
-            cout << messageInputNumber;
-            cin >> a;
-            cin >> b;
+            std::cout << "Введите первую дробь в формате <числитель знаменатель>: ";
+            std::cin >> num1 >> denom1;
 
-            cout << messageResult << NOK(a, b);
-            break;
-        case '3':
-            cout << "Введите число, которое вы хотите разложить на множители: ";
-            cin >> num;
-            cout << Factorization(num);
+            if (HandleInputError(messageErrorInput) || CheckNonZeroDenominator(denom1)) {
+                return 1;
+            }
 
-            break;
-        default:
-            cout << messageError;
-            return 1;
-        }
-        return 0;
+            std::cout << "Введите вторую дробь в формате <числитель знаменатель>: ";
+            std::cin >> num2 >> denom2;
 
-    case '2':
-        //if (пустая строка или неверный тип введенных данных, выводить ошибку и заканчивать программу)
-        //else cout... switch case:
-        cout << "Введите первую дробь в формате <числитель знаменатель>: ";
-        cin >> num1 >> denom1;
+            if (HandleInputError(messageErrorInput) || CheckNonZeroDenominator(denom2)) {
+                return 1;
+            }
 
-        cout << "Введите вторую дробь в формате <числитель знаменатель>: ";
-        cin >> num2 >> denom2;
 
-        pair<int, int> fraction1 = { num1, denom1 };
-        pair<int, int> fraction2 = { num2, denom2 };
+            pair<int, int> fraction1 = { num1, denom1 };
+            pair<int, int> fraction2 = { num2, denom2 };
 
-        pair<int, int> result;
+            pair<int, int> result;
 
-        char operationFrac;
-        cout << "Выберите операцию (+, -, *, /): ";
-        cin >> operationFrac;
+            char operationFraction;
+            std::cout << "Выберите операцию (+, -, *, /): ";
+            std::cin >> operationFraction;
 
-        switch (operationFrac) {
-        case '+':
-            result = addFractions(fraction1, fraction2);
-            break;
-        case '-':
-            result = subtractFractions(fraction1, fraction2);
-            break;
-        case '*':
-            result = multiplyFractions(fraction1, fraction2);
-            break;
-        case '/':
-            result = divideFractions(fraction1, fraction2);
-            break;
-        default:
-            cout << messageError;
-            return 1;
-        }
-
-        cout << messageResult << result.first << "/" << result.second << endl;
-
-        return 0;
+            switch (operationFraction) {
+            case '+':
+                result = addFractions(fraction1, fraction2);
+                break;
+            case '-':
+                result = subtractFractions(fraction1, fraction2);
+                break;
+            case '*':
+                result = multiplyFractions(fraction1, fraction2);
+                break;
+            case '/':
+                result = divideFractions(fraction1, fraction2);
+                break;
+            default:
+                std::cout << messageError;
+            }
+            std::cout << "Результат: " << result.first << "/" << result.second << endl;
     }
+
+    return 0;
 }
+
+
